96aca99dd820219664f68930a50207d3278c95dc
Jiang Li <115767656+dellixx@users.noreply.github.com>
2025-03-25 17:46:52 +0800

    typos

--- biokg/examples.sh
+++ biokg/examples.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #######################################################################
-# TransQR-1000
+# TransERR-1000
 python run.py --do_train --cuda --do_valid --do_test --evaluate_train \
-  --model TransQR -n 128 -b 1024 -d 1000 -g 15 -a 1.0 -adv -tr \
+  --model TransERR -n 128 -b 1024 -d 1000 -g 15 -a 1.0 -adv -tr \
   -lr 0.0001 --max_steps 600000 --cpu_num 8 --test_batch_size 32
 
 ######################################################################
 
 #######################################################################
-# TransQR-2000
+# TransERR-2000
 # python run.py --do_train --cuda --do_valid --do_test --evaluate_train \
-#   --model TransQR -n 128 -b 1024 -d 2000 -g 20 -a 1.0 -adv -tr \
+#   --model TransERR -n 128 -b 1024 -d 2000 -g 20 -a 1.0 -adv -tr \
 #   -lr 0.00005 --max_steps 600000 --cpu_num 8 --test_batch_size 32
 
 ######################################################################
